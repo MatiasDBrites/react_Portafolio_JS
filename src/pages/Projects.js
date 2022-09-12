1a80b90ea7ec40d151d94c3b8e8117f2482f1940
MatiasDBrites <matias.brites@gmail.com>
2022-09-12 01:02:16 -0500

    animacion de botones, cambio en pryectos

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,15 +1,14 @@
 import React from "react";
-import TitlebarImageList from "../components/Nav/projects/PortProject";
 import video from "../components/Nav/homemain/video/video6.mp4"
+import Projects from "../components/Nav/projects/PortProject";
 
 const Proyectos = () => {
   return (
     <div className="proyectos__container">
-      <h1>Proyectos</h1>
       <video autoPlay="on"  muted>
         <source type="video/webm" src={video}></source>
       </video>
-      <TitlebarImageList></TitlebarImageList>
+      <Projects/>
     </div>
   )
 }
