8b23cc9b42f07541994faaadec703389b35ec202
Matias D Brites <104856701+MatiasDBrites@users.noreply.github.com>
2022-09-20 12:48:10 -0500

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,7 @@ import video from "../components/Nav/homemain/video/video6.mp4"
 const Contacto = () => {
   return (
     <div className="about__container">
-      <h1>Contacto</h1>
+      <h1>Contact</h1>
       <video autoPlay="on"  muted>
         <source type="video/webm" src={video}></source>
       </video>
